410dea2432354222467091836ecbd1f5694a25af
Aryaneladeira <aryanemottals@gmail.com>
2020-08-06 19:11:05 -0300

    versão 0.1

---
+++ script.js
@@ -0,0 +1,9 @@
+var mus = document.getElementById('aud')
+
+function tocar1(){
+        mus.setAttribute('src', 'arquivos/Eternal.mp3')
+}
+
+function tocar2(){
+        mus.setAttribute('src', 'arquivos/fung-sway-lofi-hiphop.mp3')
+}
